d1ef916e930e875482c49a69d3ea0de5ef667a00
Gavin <gsnyder@stevens.edu>
2021-04-07 12:05:35 -0400

    setup bootstrap

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/home')
-def hello():
-    return 'Hello'
+@app.route('/buy')
+def displayBuyerTab():
+    return render_template('buyerTab.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
