7ecbf36787517ed657ba4596111c3f4b0121627e
Gavin <gsnyder@stevens.edu>
2021-04-07 12:34:55 -0400

    final setup

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def index():
+def displayIndex():
     return render_template('index.html')
 
 @app.route('/buy')
 def displayBuyerTab():
     return render_template('buyerTab.html')
+
+@app.route('/sell')
+def displaySellerTab():
+    return render_template('sellerTab.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
