879becf8c9783b9205ad87d3bf52845b20cc65c3
pdamiano-11 <pdamiano@stevens.edu>
2021-04-10 22:52:05 -0400

    Added post and pull, and updated search bar

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import db_func
+import classes
 
 app = Flask(__name__)
 
@@ -9,13 +11,26 @@ app = Flask(__name__)
 def displayIndex():
     return render_template('index.html')
 
-@app.route('/buy')
+@app.route('/buy', methods = ['GET', 'POST'])
 def displayBuyerTab():
     return render_template('buyerTab.html')
 
 @app.route('/sell')
 def displaySellerTab():
     return render_template('sellerTab.html')
+
+@app.route('/itemlist', methods = ['GET', 'POST'])
+def displayListings():
+    typ = ''
+    keyword = ''
+    if request.method == 'POST':
+        typ = request.form['type']
+        keyword = request.form['choice']
+    res = db_func.pull(typ, keyword)
+    li = []
+    for x in res:
+        li.append(x)
+    return li[0]['Name']
 
 
 
